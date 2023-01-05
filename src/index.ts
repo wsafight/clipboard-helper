c81558f6faf7b6784a84b1967789583c00c21723
wsafight <984292420@qq.com>
2023-01-06 00:31:50 +0800

    #FEAT: add image

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import ClipboardJs from 'clipboard'
+import { convertImageToBlob } from './utils'
 
-const copyToClipboard = (textToCopy: string) => {
+const copyTextToClipboard = (textToCopy: string) => {
   return new Promise(function (resolve, reject) {
     const fakeBtn = document.createElement('button')
 
@@ -19,11 +20,38 @@ const copyToClipboard = (textToCopy: string) => {
       clipboard.destroy()
       reject(e)
     })
-    
+
     fakeBtn.click()
   })
 }
 
+const copyImageToClipboard = async (img: string | HTMLImageElement) => {
+  if (!img || (img !== 'string' && !(img instanceof HTMLImageElement))) {
+    return Promise.reject('image must be a string or HTMLImageElement')
+  }
+
+  if (img === 'string') {
+    return fetch(img).then(data => {
+      return data.blob()
+    }).then(blob => {
+      return navigator.clipboard.write([
+        new ClipboardItem({
+          [blob.type]: blob
+        })
+      ])
+    })
+  }
+
+  const blobFromImg =  convertImageToBlob(img)
+
+  return navigator.clipboard.write([
+    new ClipboardItem({
+      [blobFromImg.type]: blobFromImg
+    })
+  ])
+}
+
 export {
-  copyToClipboard
+  copyTextToClipboard,
+  copyImageToClipboard,
 }
---
+++ src/utils.ts
@@ -0,0 +1,39 @@
+const convertImageToCanvas = (image: HTMLImageElement, width: number, height: number): HTMLCanvasElement => {
+  const canvas: HTMLCanvasElement = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  canvas.getContext('2d')?.drawImage(image, 0, 0)
+  return canvas
+}
+
+
+const base64ToBlob = (urlData: string, type: string): Blob => {
+  const [mimeInfo, content] = urlData.split(',')
+  let mime = type
+  if (mimeInfo) {
+    mime = mimeInfo.match(/:(.*?);/)?.[1] || type;
+  }
+  const bytes = window.atob(content)
+  const ab = new ArrayBuffer(bytes.length)
+  const ia = new Uint8Array(ab)
+  for (let index = 0; index < bytes.length; index++) {
+    ia[index] = bytes.charCodeAt(index)
+  }
+  return new Blob([ab], { type: mime })
+};
+
+
+/**
+ * 此函数用来处理图片格式的转化
+ * @param img <img src="">用来转化的原图片
+ * @param type 转化的格式类型
+ * @return blob
+ */
+export const convertImageToBlob = (
+  img: HTMLImageElement,
+  type: string = 'png'
+): Blob => {
+  const { naturalHeight: height, naturalWidth: width } = img
+  const canvas = convertImageToCanvas(img, width, height)
+  return base64ToBlob(canvas.toDataURL(`image/${type}`), type)
+}

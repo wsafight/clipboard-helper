b8a78a0f167d28159ed75882cea30a9b846f1635
wsafight <984292420@qq.com>
2023-01-06 00:37:03 +0800

    #FEAT: add support

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import ClipboardJs from 'clipboard'
 import { convertImageToBlob } from './utils'
 
 const copyTextToClipboard = (textToCopy: string) => {
+
+  if (!ClipboardJs.isSupported()) {
+    return Promise.reject('The current browser does not support this feature')
+  }
+
   return new Promise(function (resolve, reject) {
     const fakeBtn = document.createElement('button')
 
@@ -26,6 +31,12 @@ const copyTextToClipboard = (textToCopy: string) => {
 }
 
 const copyImageToClipboard = async (img: string | HTMLImageElement) => {
+  const clipboard = navigator.clipboard
+
+  if (!clipboard) {
+    return Promise.reject('The current browser does not support this feature')
+  }
+
   if (!img || (img !== 'string' && !(img instanceof HTMLImageElement))) {
     return Promise.reject('image must be a string or HTMLImageElement')
   }
@@ -34,7 +45,7 @@ const copyImageToClipboard = async (img: string | HTMLImageElement) => {
     return fetch(img).then(data => {
       return data.blob()
     }).then(blob => {
-      return navigator.clipboard.write([
+      return clipboard.write([
         new ClipboardItem({
           [blob.type]: blob
         })
@@ -44,7 +55,7 @@ const copyImageToClipboard = async (img: string | HTMLImageElement) => {
 
   const blobFromImg =  convertImageToBlob(img)
 
-  return navigator.clipboard.write([
+  return clipboard.write([
     new ClipboardItem({
       [blobFromImg.type]: blobFromImg
     })
